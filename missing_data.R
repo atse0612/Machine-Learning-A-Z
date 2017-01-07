8887da15dea5e3ae1fc211f023e8f283b600922e
atse0612 <atse0612@users.noreply.github.com>
2017-01-07 08:34:04 -0800

    Add files via upload

---
+++ missing_data.R
@@ -0,0 +1,17 @@
+# Data Preprocessing
+
+# Importing the dataset
+dataset = read.csv('Data.csv')
+
+# Taking care of missing data
+dataset$Age = ifelse(is.na(dataset$Age),
+                     ave(dataset$Age, FUN = function(x) mean(x, na.rm = TRUE)),
+                     dataset$Age)
+dataset$Salary = ifelse(is.na(dataset$Salary),
+                        ave(dataset$Salary, FUN = function(x) mean(x, na.rm = TRUE)),
+                        dataset$Salary)
+
+# Running the Data
+
+dataset
+
