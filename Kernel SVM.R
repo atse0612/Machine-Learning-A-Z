2b5f33b6fbfcb1d7150f56608218d69b6133cafc
atse0612 <atse0612@users.noreply.github.com>
2017-02-26 14:41:27 -0800

    Add files via upload

---
+++ Kernel SVM.R
@@ -0,0 +1,75 @@
+# Kernel SVM
+
+### Changing the Working Directory
+
+setwd('./Machine Learning A-Z/Part 3 - Classification/Section 17 - Kernel SVM')
+
+## Importing the Dataset
+
+dataset = read.csv('Social_Network_Ads.csv')
+dataset = dataset[3:5]
+
+## Splitting the Dataset into the Training Set and Test Set
+
+library(caTools)
+set.seed(123)
+split = sample.split(dataset$Purchased, SplitRatio = 0.75)
+training_set = subset(dataset, split == TRUE)
+test_set = subset(dataset, split == FALSE)
+
+## Feature Scaling
+
+training_set[-3] = scale(training_set[-3])
+test_set[-3] = scale(test_set[-3])
+
+## Fitting classifier to the Training Set
+
+library(e1071)
+
+classifier = svm(formula = Purchased ~ .,
+                 data = training_set,
+                 type = 'C-classification',
+                 kernel = 'radial')
+
+## Predicting the Test Set Results
+
+y_pred = predict(classifier, newdata = test_set[-3])
+
+## Making the Confusion Matrix
+
+cm = table(test_set[,3], y_pred)
+cm
+
+## Visualizing the Training Set Results
+
+library(ElemStatLearn)
+set = training_set
+X1 = seq(min(set[, 1]) - 1, max(set[, 1]) + 1, by = 0.01)
+X2 = seq(min(set[, 2]) - 1, max(set[, 2]) + 1, by = 0.01)
+grid_set = expand.grid(X1, X2)
+colnames(grid_set) = c('Age', 'EstimatedSalary')
+y_grid = predict(classifier, newdata = grid_set)
+plot(set[, -3],
+     main = 'Kernel SVM (Training Set)',
+     xlab = 'Age', ylab = 'Estimated Salary',
+     xlim = range(X1), ylim = range(X2))
+contour(X1, X2, matrix(as.numeric(y_grid), length(X1), length(X2)), add = TRUE)
+points(grid_set, pch = '.', col = ifelse(y_grid == 1, 'salmon', 'blue'))
+points(set, pch = 21, bg = ifelse(set[, 3] == 1, 'green4', 'red3'))
+
+## Visualizing the Test Set Results
+
+
+set = test_set
+X1 = seq(min(set[, 1]) - 1, max(set[, 1]) + 1, by = 0.01)
+X2 = seq(min(set[, 2]) - 1, max(set[, 2]) + 1, by = 0.01)
+grid_set = expand.grid(X1, X2)
+colnames(grid_set) = c('Age', 'EstimatedSalary')
+y_grid = predict(classifier, newdata = grid_set)
+plot(set[, -3],
+     main = 'Kernel SVM (Test Set)',
+     xlab = 'Age', ylab = 'Estimated Salary',
+     xlim = range(X1), ylim = range(X2))
+contour(X1, X2, matrix(as.numeric(y_grid), length(X1), length(X2)), add = TRUE)
+points(grid_set, pch = '.', col = ifelse(y_grid == 1, 'khaki', 'maroon'))
+points(set, pch = 21, bg = ifelse(set[, 3] == 1, 'blue', 'orange'))
