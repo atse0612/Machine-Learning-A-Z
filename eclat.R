31f8153ca71f4753ef6ee0e8cb293495605b612a
A. Tse <atse0612@users.noreply.github.com>
2017-05-24 17:57:48 -0700

    Add files via upload

---
+++ eclat.R
@@ -0,0 +1,20 @@
+# Eclat
+
+## Setting the Working Directory
+
+setwd('./Machine Learning A-Z/Part 5 - Association Rule Learning/Section 29 - Eclat')
+
+
+# Data Preprocessing
+# install.packages('arules')
+library(arules)
+dataset = read.csv('Market_Basket_Optimisation.csv')
+dataset = read.transactions('Market_Basket_Optimisation.csv', sep = ',', rm.duplicates = TRUE)
+summary(dataset)
+itemFrequencyPlot(dataset, topN = 10)
+
+# Training Eclat on the dataset
+rules = eclat(data = dataset, parameter = list(support = 0.003, minlen = 2))
+
+# Visualising the results
+inspect(sort(rules, by = 'support')[1:10])
